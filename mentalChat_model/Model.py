f6189444d20ae35e47d54fbc5f550b05e70d2875
SasmitDey <12322136.st@gmail.com>
2024-04-13 01:01:35 +0530

    deleted index.html outisde template

--- mentalChat_model/Model.py
+++ mentalChat_model/Model.py
@@ -15,10 +15,27 @@ logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
 """convokit to get corpuses"""
 from convokit import Corpus, download
-corpus = Corpus(filename=download("subreddit-depression"))
+# corpus = Corpus(filename=download("subreddit-depression"))
 
 """Importing flask"""
 from flask import Flask, render_template, request
+
+
+
+
+chatbot = ChatBot("mentalChat")
+trainer = ChatterBotCorpusTrainer(chatbot)
+list_trainer = ListTrainer(chatbot)
+# trainer.export_for_training("./my_export.json")
+
+# trainer.train("Training\mentalhealth.json")
+# trainer.train("")
+flag = 1
+# while(flag==1):
+#     myInput = input("You: ")
+#     response = chatbot.get_response(myInput)
+#     print(response)
+
 
 app = Flask(__name__)
 @app.route("/")
@@ -29,17 +46,4 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.get_response(userText))
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)    
-
-
-chatbot = ChatBot("mentalChat")
-trainer = ChatterBotCorpusTrainer(chatbot)
-list_trainer = ListTrainer(chatbot)
-# trainer.export_for_training("./my_export.json")
-
-# trainer.train("Training\mentalhealth.json")
-# trainer.train("")
-while(True):
-    myInput = input("You: ")
-    response = chatbot.get_response(myInput)
-    print(response)
+    app.run()    
